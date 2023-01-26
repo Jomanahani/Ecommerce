9642a06bf2960b81b5f48e49bef6bff5aa92af4a
Jomanahani <jomanahani123456@gmail.com>
2023-01-26 21:47:56 +0200

    add Quote section with form

---
+++ src/Sections/QuoteRequest/index.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+
+import quote from "../../Assetse/quote.png";
+import QuoteForm from "../../Components/QuoteForm";
+
+const QuoteContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 93%;
+  margin: 2rem auto;
+  background-image: url(${quote});
+  background-size: cover;
+  border: 1px solid ${(props) => props.theme.pallet.Lines};
+  border-radius: 0.5rem;
+`;
+const InnerContainer = styled.div`
+  width: 40%;
+  padding: 3rem;
+  color: ${(props) => props.theme.pallet.mainBackdround};
+  & > h3 {
+    font-size: 2rem;
+  }
+  & > p {
+    font-size: 1rem;
+    padding: 1rem 0;
+  }
+`;
+
+export default function QuoteRequest() {
+  return (
+    <section>
+      <QuoteContainer>
+        <InnerContainer>
+          <h3>An easy way to send requests to all suppliers</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt.
+          </p>
+        </InnerContainer>
+        <QuoteForm />
+      </QuoteContainer>
+    </section>
+  );
+}
